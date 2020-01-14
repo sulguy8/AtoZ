563390e66135475eb587d45ebb036ee78b95dfa9
Administrator <Administrator@koitt03a-PC>
2020-01-14 18:31:45 +0900

    0114

---
+++ src/homework/Array.java
@@ -0,0 +1,16 @@
+package homework;
+
+public class Array {
+	static int[][] nums;
+	
+	public static void main(String[] args) {
+		
+		for(int i=1; i<5; i++) {
+			nums = new int[i][i];
+		}
+		
+		for(int i=0;i<nums.length;i++) {
+			System.out.print(nums[i][a] = a);
+		}
+	}
+}
